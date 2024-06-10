01a8d26572ed8c9e94631011e9b2e814894b93e8
luccasfto <luccasfto@gmail.com>
2024-06-10 20:02:53 -0300

    adicionado header e footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,11 @@ import Image from "next/image";
 import variable from "./styles/variables.module.scss";
 
 export default function Home() {
-  
+
   return (
     <>
       <main>
-        <p className={variable.test}>Test</p>
+        <p className={variable.test}>Test22</p>
       </main>
     </>
   );
---
+++ src/ui/home/footer.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Footer() {
+
+  return (
+    <>
+      <main>
+        <p>Test32</p>
+      </main>
+    </>
+  );
+}
